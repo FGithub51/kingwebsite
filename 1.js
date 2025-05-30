186602c3448a4b2f5f8d99a5c7af855804913c07
FGithub51 <147294862+FGithub51@users.noreply.github.com>
2025-05-30 23:02:50 +0800

    all files

--- 1.js
+++ 1.js
@@ -21,6 +21,7 @@ new TradingView.widget(
         "show_popup_button": true,
         "popup_width": "1000",
         "popup_height": "650"
+        
     }
 );
 
@@ -38,30 +39,3 @@ new TradingView.widget2(
         "container_id": "meter",
     }
 );
-
-function scrollToElement(elementSelector, instance = 0) {
-    // Select all elements that match the given selector
-    const elements = document.querySelectorAll(elementSelector);
-    // Check if there are elements matching the selector and if the requested instance exists
-    if (elements.length > instance) {
-        // Scroll to the specified instance of the element
-        elements[instance].scrollIntoView({ behavior: 'smooth' });
-    }
-}
-
-const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
-const link3 = document.getElementById("link3");
-
-link1.addEventListener('click', () => {
-    scrollToElement('.header');
-});
-
-link2.addEventListener('click', () => {
-    // Scroll to the second element with "header" class
-    scrollToElement('.header', 1);
-});
-
-link3.addEventListener('click', () => {
-    scrollToElement('.column');
-});
